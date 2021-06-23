1ff961f7554a915b2eb8c40d4b72287756ee022a
itwaky <71080602+itwaky@users.noreply.github.com>
2021-06-23 15:13:40 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `http://newsite/wordpress/graphql`,
+          `https://agencecgr.000webhostapp.com/graphql`,
       },
     },
 
